7577dc7b8311f0778654e04666798e3582801821
David Brochart <david.brochart@gmail.com>
2019-02-27 07:48:35 +0100

    Masks are in a DataArray along the label dimension

--- py/mask2pangeo.py
+++ py/mask2pangeo.py
@@ -4,7 +4,9 @@ import os
 from tqdm import tqdm
 import shutil
 import subprocess
+import zarr
 
+compressor = zarr.Blosc(cname='zstd', clevel=3, shuffle=2)
 shutil.rmtree('tmp/ds_mask', ignore_errors=True)
 shutil.rmtree('tmp/ds_mask_new', ignore_errors=True)
 labels = os.listdir('ws_mask/amazonas')
@@ -33,22 +35,22 @@ for d in array_dims:
         coord[d] = np.arange(vmin[d], vmax[d]+tolerance, pix_deg)
     else:
         coord[d] = np.arange(vmax[d], vmin[d]-tolerance, -pix_deg)
-nlat = int(round((coord['lat'][0] - coord['lat'][-1]) / pix_deg)) + 1
-nlon = int(round((coord['lon'][-1] - coord['lon'][0]) / pix_deg)) + 1
+max_len = max([len(label) for label in list(arrays)])
 for label in tqdm(arrays):
     da = arrays[label]
-    a = np.zeros((nlat, nlon), dtype=np.uint8)
+    a = np.zeros((1, coord['lat'].shape[0], coord['lon'].shape[0]), dtype=np.uint8)
     dlat = int(round((coord['lat'][0] - da.lat.values[0]) / pix_deg))
     dlon = int(round((da.lon.values[0] - coord['lon'][0]) / pix_deg))
-    a[dlat:dlat+da.shape[0], dlon:dlon+da.shape[1]] = da.values
-    attrs = da.attrs
-    attrs['bbox'] = ((da.lat.values[0]+pix_deg/2, da.lon.values[0]-pix_deg/2), (da.lat.values[-1]-pix_deg/2, da.lon.values[-1]+pix_deg/2))
-    da_reindex = xr.DataArray(a, coords=[coord['lat'], coord['lon']], dims=['lat', 'lon'], attrs=attrs)
-    ds = da_reindex.to_dataset(name=label)
-    ds.to_zarr('tmp/ds_mask_new')
+    a[0, dlat:dlat+da.shape[0], dlon:dlon+da.shape[1]] = da.values
+    da_reindex = xr.DataArray(a, coords=[[label], coord['lat'], coord['lon']], dims=['label', 'lat', 'lon'])
+    ds = da_reindex.to_dataset(name='mask')
     if os.path.exists('tmp/ds_mask'):
-        subprocess.check_call(f'cp -r tmp/ds_mask_new/{label} tmp/ds_mask/{label}'.split())
         shutil.rmtree('tmp/ds_mask_new', ignore_errors=True)
+        ds.to_zarr('tmp/ds_mask_new', encoding={'label': {'dtype': f'|S{max_len}'}, 'mask': {'compressor': compressor}})
+        ds_mask = zarr.open('tmp/ds_mask', mode='a')
+        ds_new = zarr.open('tmp/ds_mask_new', mode='r')
+        ds_mask['mask'].append(ds_new['mask'])
+        ds_mask['label'].append(ds_new['label'])
     else:
-        os.rename('tmp/ds_mask_new', 'tmp/ds_mask')
+        ds.to_zarr('tmp/ds_mask', encoding={'label': {'dtype': f'|S{max_len}'}, 'mask': {'compressor': compressor}})
 subprocess.check_call('gsutil -m cp -r tmp/ds_mask/ gs://pangeo-data/amazonas/'.split())
